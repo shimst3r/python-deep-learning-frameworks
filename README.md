ea7f0bead970b93d22b7c800cc7f55474a5d5562
shimst3r <shimst3r@gmail.com>
2021-09-04 16:24:19 +0200

    First working prototype :)

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
 # [Python Deep Learning Frameworks](https://www.github.com/shimst3r/python-deep-learning-frameworks)
 
 Python Deep Learning Frameworks is a list of deep learning frameworks that are implemented in Python and hosted on GitHub. It is intended as an entry point for people new to deep learning in Python. The project is inspired by @mingrammer's [Top Python Web Frameworks](https://github.com/mingrammer/python-web-framework-stars).
+
+## The List
+
+| Project Name | Project Organization | Description | Stars | Forks | Open Issues | Last Commit |
+| ------------ | -------------------- | ----------- | ----: | ----: | ----------: | ----------- |
+
+| tensorflow | None | An Open Source Machine Learning Framework for Everyone | 158847 | 85414 | 3375 | 0 day(s) ago |
+
+| keras | Keras | Deep Learning for humans | 52278 | 18803 | 401 | 0 day(s) ago |
+
+| transformers | Hugging Face | 🤗 Transformers: State-of-the-art Natural Language Processing for Pytorch, TensorFlow, and JAX. | 50687 | 12037 | 408 | 0 day(s) ago |
+
+| pytorch | None | Tensors and Dynamic neural networks in Python with strong GPU acceleration | 50608 | 13808 | 9725 | 0 day(s) ago |
+
+| caffe | Berkeley Vision and Learning Center | Caffe: a fast open framework for deep learning. | 31891 | 18877 | 1168 | 0 day(s) ago |
+
+| incubator-mxnet | The Apache Software Foundation | Lightweight, Portable, Flexible Distributed/Mobile Deep Learning with Dynamic, Mutation-aware Dataflow Dep Scheduler; for Python, R, Julia, Scala, Go, Javascript and more | 19628 | 6875 | 1935 | 0 day(s) ago |
+
+
+## Contributing
+
+If you want to add a new framework to the list, please add it to `list.txt` and open a PR!
--- python_deep_learning_frameworks/update.py
+++ python_deep_learning_frameworks/update.py
@@ -1,9 +1,81 @@
+import math
 import os
+import sys
+from datetime import datetime, timedelta
+from pathlib import Path
 
 from github import Github
+from jinja2 import Template
 
-if __name__ == '__main__':
-    access_token = os.environ['GITHUB_TOKEN']
+PROJECT_ROOT = Path(__file__).parent
+DATETIME_IN = "%a, %d %b %Y %H:%M:%S %Z"
+
+
+class Repository:
+    def __init__(self, repo):
+        self.repo = repo
+
+    @property
+    def name(self):
+        return self.repo.name
+
+    @property
+    def organization_name(self):
+        return self.repo.organization.name
+
+    @property
+    def description(self):
+        return self.repo.description
+
+    @property
+    def stargazers_count(self):
+        return self.repo.stargazers_count
+
+    @property
+    def forks_count(self):
+        return self.repo.forks_count
+
+    @property
+    def open_issues_count(self):
+        return self.repo.open_issues_count
+
+    @property
+    def last_commit(self):
+        last_modified = datetime.strptime(self.repo.last_modified, DATETIME_IN)
+        delta = datetime.today() - last_modified
+        last_modified_in_days = delta / timedelta(days=1)
+
+        return math.floor(last_modified_in_days)
+
+
+def get_repositories(g):
+    with open(PROJECT_ROOT / "list.txt", "r", encoding="utf8") as in_file:
+        repo_identifiers = [line.strip() for line in in_file.readlines()]
+    repositories = [
+        Repository(repo=g.get_repo(identifier)) for identifier in repo_identifiers
+    ]
+    return repositories
+
+
+def populate_template(repositories):
+    with open(PROJECT_ROOT / "template.md.j2", "r", encoding="utf8") as template_file:
+        template = Template(template_file.read())
+    return template.render(repositories=repositories)
+
+def update_readme(template):
+    with open(PROJECT_ROOT / '../README.md', 'w', encoding='utf8') as readme_file:
+        readme_file.write(template)
+
+def main():
+    access_token = os.environ["GITHUB_TOKEN"]
     g = Github(access_token)
-    repo = g.get_repo('shimst3r/python-deep-learning-frameworks')
-    print(repo.name)
+
+    repositories = sorted(
+        get_repositories(g), key=lambda repo: repo.stargazers_count, reverse=True
+    )
+    template = populate_template(repositories)
+    update_readme(template)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
